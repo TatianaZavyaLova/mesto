c37f8823d89a28f3feb4516575813fe27a2bf467
Tanya Zavyalova <benita80@yandex.ru>
2022-05-27 03:10:28 +0300

    ПР4

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+const editButton = document.querySelector('.profile__edit-button')
+const popup = document.querySelector('.popup')
+const clousePopupButton = document.querySelector('.popup__clouse')
+const addButton = document.querySelector('.profile__add-button')
+const profileName = document.querySelector('.profile__title')
+const profileJob = document.querySelector('.profile__subtitle')
+const formElement = document.querySelector('.popup__form')
+const nameInput = document.querySelector('.popup__input_name')
+const jobInput = document.querySelector('.popup__input_job')
+
+function popupOpen() {
+  popup.classList.add('popup_opened');
+}
+
+editButton.addEventListener('click', popupOpen)
+
+function popupClouse() {
+  popup.classList.remove('popup_opened');
+}
+
+clousePopupButton.addEventListener('click', popupClouse)
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  popupClouse();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);

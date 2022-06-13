31ed269aca3e2c36f84b94f4a887b32e802a444a
Tanya Zavyalova <benita80@yandex.ru>
2022-06-13 17:17:16 +0300

    черновик

--- scripts/index.js
+++ scripts/index.js
@@ -10,15 +10,17 @@ const jobInput = document.querySelector('.popup__input_type_job')
 const addPopup = document.querySelector('.add-popup')
 const clouseAddPopupButton = document.querySelector('.add-popup__clouse')
 const cardTitle = document.querySelector('.card__title')
-const cardImage = document.querySelector('.card__image');
+const cardImage = document.querySelector('.card__image')
 const addFormElement = document.querySelector('.add-popup__form')
 const addCardInput = document.querySelector('.add-popup__input_type_place')
 const addCardImage = document.querySelector('.add-popup__input_type_image')
-const cardElement = document.querySelector('.cards');
-const templateElement = document.querySelector('.cards-template');
-const like = document.querySelector('.card__like');
-const likeActive = document.querySelector('.card__like_active');
+const cardElement = document.querySelector('.cards')
+const templateElement = document.querySelector('.cards-template')
+const like = document.querySelector('.card__like')
+const likeActive = document.querySelector('.card__like_active')
 const imagePopup = document.querySelector('image-popup')
+const clouseImagePopupButton = document.querySelector('.image-popup__clouse')
+const imagePopupPhoto = document.querySelector('.image-popup__image')
 const initialCards = [
   {
     name: 'Архыз',
@@ -74,7 +76,7 @@ function addCard(name, link) {
 function handlerCardSubmit(e) {
   e.preventDefault();
   const name = addCardInput.value;
-  const link = addCardImage.src;
+  const link = addCardImage.value;
   addCard(name, link);
   //initialCards(reset);
   addPopupClouse();
@@ -88,13 +90,21 @@ function imagePopupOpen() {
   imagePopup.classList.add('image-popup_opened');
 }
 
-//card.addEventListener('click', imagePopupOpen);
+//cardImage.addEventListener('click', imagePopupOpen)
+
+
+//function imagePopupClouse() {
+  //imagePopup.classList.remove('image-popup_opened');
+//}
+
+//clouseImagePopupButton.addEventListener('click', imagePopupClouse)
+
 
 function addPopupOpen() {
   addPopup.classList.add('add-popup_opened');
 }
 
-addButton.addEventListener('click', addPopupOpen)
+addButton.addEventListener('click', addPopupOpen);
 
 function addPopupClouse() {
   addPopup.classList.remove('add-popup_opened');
